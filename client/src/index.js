9144b3e6b233dd3309b6d2ce994f7c224aeb6ec2
dulice <duliceellen600@gmail.com>
2022-05-30 17:47:36 +0630

    loading

--- client/src/index.js
+++ client/src/index.js
@@ -1,15 +1,20 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+// import App from './App';
 import 'react-toastify/dist/ReactToastify.css';
 import { BrowserRouter } from 'react-router-dom';
 import { StoreProvider } from './context/Store';
 import { HelmetProvider } from 'react-helmet-async'
+import Loading from './components/Loading';
+
+const App = React.lazy(() => import('./App'));
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+
   <React.StrictMode>
+    <Suspense fallback={<Loading />}>
     <HelmetProvider>
       <StoreProvider>
         <BrowserRouter>
@@ -17,6 +22,7 @@ root.render(
         </BrowserRouter>
       </StoreProvider>
     </HelmetProvider>
+    </Suspense>
   </React.StrictMode>
 );
 
