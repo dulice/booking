9c6b28fadde8c857109ca3b20bd51fea3a58f3b8
dulice <duliceellen600@gmail.com>
2022-05-31 08:16:05 +0630

    image link

--- client/src/components/Room.jsx
+++ client/src/components/Room.jsx
@@ -6,8 +6,8 @@ const Room = (props) => {
     const { room } = props
   return (
     <>
-        <img src={room.image[0]} alt="" className='h-4/6 w-full hover:scale-105 duration-500'/>
         <Link to={`/rooms/${room._id}`}>
+          <img src={room.image[0]} alt="" className='h-4/6 w-full hover:scale-105 duration-500'/>
           <p className='capitalize hover:text-blue-500 font-bold mt-3'>{room.name}</p>
         </Link>
         <p className='my-3'>Starting From <span className='font-bold text-blue-600'> ${room.price} </span></p>
